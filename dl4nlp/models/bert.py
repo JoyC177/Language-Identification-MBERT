131d7afd3e52a2fc5bbefcb16e011354b4cda162
Konrad Bereda <0darnox0@gmail.com>
2022-10-02 12:26:22 +0200

    Add Bertic model

--- dl4nlp/models/bert.py
+++ dl4nlp/models/bert.py
@@ -1,33 +1,30 @@
-from typing import Tuple
-
 import torch
 from torch.utils.data import Dataset
 from tqdm.auto import tqdm
-from transformers import BertModel, BertTokenizer
-from transformers import XLMRobertaTokenizer, XLMRobertaModel
+from transformers import AutoModel, AutoTokenizer
 
 from ..utils import DEVICE
 
 MBERT_MODEL = "bert-base-multilingual-cased"
 XLMBERT_MODEL = "xlm-roberta-base"
+BERTIC_MODEL = "classla/bcms-bertic"
 EMBEDDINGS_DIR = "embeddings/"
 NUM_LAYERS = 12
 
 
 def generate_bert_embeddings(
-    model: BertModel, tokenizer: BertTokenizer, dataset: Dataset, batch_size: int = 4
+    model, tokenizer, dataset: Dataset, batch_size: int = 4
 ) -> torch.Tensor:
     model.eval()
     data_loader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, shuffle=False
     )
-
     with torch.no_grad():
         embeddings = []
         # Loop over the sentences in batches
         for sentences_batch, _ in tqdm(data_loader):
             encoded_input = tokenizer(
-                sentences_batch,
+                list(sentences_batch),
                 padding=True,
                 truncation=True,
                 return_tensors="pt",
@@ -36,22 +33,15 @@ def generate_bert_embeddings(
 
             # Take [CLS] token embedding of specific layers or the last one.
             cls_embeddings = torch.stack(output.hidden_states)[:, :, 0, :].cpu()
-            
+
             # Store the embeddings
             embeddings.append(cls_embeddings)
 
     return torch.concat(embeddings, dim=1)
 
 
-def load_model(model_name: str = MBERT_MODEL) -> Tuple[BertModel, BertTokenizer]:
-    if model_name == MBERT_MODEL:
-        tokenizer = BertTokenizer.from_pretrained(model_name)
-        model = BertModel.from_pretrained(model_name, output_hidden_states=True).to(DEVICE)
-    elif model_name == XLMBERT_MODEL:
-        tokenizer = XLMRobertaTokenizer.from_pretrained(model_name)
-        model = XLMRobertaModel.from_pretrained(model_name, output_hidden_states=True).to(DEVICE)
-    else:
-        raise ValueError("Invalid bert-model-name was provided,  \
-            try with 'xlm-roberta-base' or 'bert-base-multilingual-cased'")
+def load_model(model_name: str = MBERT_MODEL):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name, output_hidden_states=True).to(DEVICE)
 
     return model, tokenizer
